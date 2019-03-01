c675754390ba8542302381ee474da4bac9654d48
zonanjaro86 <zonanjaro86@gmail.com>
2019-03-01 14:53:18 +0900

    文字列からノードに変換する関数追加

--- js/multiReplace.js
+++ js/multiReplace.js
@@ -35,9 +35,17 @@ const init = () => {
  * @function init
  */
 const createDom = () => {
-    const node = document.createElement('h1');
-    node.innerText = 'test';
-    document.body.appendChild(node);
+    const nodes = [];
+
+    // test
+    nodes.push(htmlToNode('<h1>test</h1>'));
+    nodes.push(htmlToNode('<p>testです</p>'));
+    
+    const fragment = document.createDocumentFragment();
+    nodes.forEach(node => {
+        fragment.appendChild(node);
+    });
+    document.body.appendChild(fragment);
 };
 
 /**
@@ -48,6 +56,24 @@ const createDom = () => {
 const setEvent = () => {
     
 };
+
+/**
+ * HTML文字列をノードへ変換
+ * 
+ * @function
+ * @param {String} htmlStr
+ * @return {NodeList}
+ */
+const htmlToNode = (htmlStr) => {
+    if (!htmlStr || typeof htmlStr !== 'string') return;
+
+    // 任意の要素のinnerHTMLとして設定し、firstChildを取り出すことで変換
+    // 複数ノードが連結した文字列では2番目以降は無視される
+    const tmpElement = document.createElement('div');
+    tmpElement.innerHTML = htmlStr;
+
+    return tmpElement.firstChild;
+}
 
 onReadyPromise()
 .then(init)
