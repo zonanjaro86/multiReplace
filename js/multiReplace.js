a032a43e44466870a5c76eaad35d1a2270e988b2
zonanjaro86 <zonanjaro86@gmail.com>
2019-03-01 11:47:12 +0900

    ページ読み込み後の非同期処理の起点となる関数を追加

--- js/multiReplace.js
+++ js/multiReplace.js
@@ -1,1 +1,21 @@
-console.log('test');
+/**
+ * ページ読み込み後の非同期処理起点
+ * 
+ * @async
+ * @function onReadyPromise
+ * @return {Promise}
+ */
+const onReadyPromise = () => {
+    return new Promise((resolve, reject) => {
+        const readyState = document.readyState;
+        if (readyState === 'interactive' || readyState == 'complete') {
+            resolve();
+        } else {
+            window.addEventListener('DOMContentLoaded', resolve);
+        }
+    });
+};
+
+onReadyPromise().then(() => {
+    console.log('loaded');
+});
