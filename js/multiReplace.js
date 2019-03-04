d2fa0d1d3169d76b4d448e661fb7a0984eba6250
zonanjaro86 <zonanjaro86@gmail.com>
2019-03-04 10:05:52 +0900

    ID, クラスを定数化

--- js/multiReplace.js
+++ js/multiReplace.js
@@ -35,24 +35,31 @@ const init = () => {
  * @function init
  */
 const createDom = () => {
+    const ID_REPLACE_STRINGS = 'replaceStrings';
+    const ID_REPLACE_TARGET = 'replaceTarget';
+    const ID_BTN_EXECUTE = 'btn_execute';
+    const CLS_REPLACE_STRING_BEFORE = 'beforeString';
+    const CLS_REPLACE_STRING_AFTER = 'afterString';
+    const CLS_REPLACE_TARGET = 'resultWrapper';
+
     const nodes = [];
 
     let sec1 = '';
     sec1 += `<section>`;
     sec1 += `<h2>置換文字列</h2>`;
-    sec1 += `<ol id="replaceStrings">`;
-    sec1 += `<li><input class="before" /><span> ⇒ </span><input class="after" /></li>`;
+    sec1 += `<ol id="${ID_REPLACE_STRINGS}">`;
+    sec1 += `<li><input class="${CLS_REPLACE_STRING_BEFORE}" /><span> ⇒ </span><input class="${CLS_REPLACE_STRING_AFTER}" /></li>`;
     sec1 += `</ol>`;
     sec1 += `</section>`;
 
     let sec2 = '';
     sec2 += `<section>`;
     sec2 += `<h2>置換対象</h2>`;
-    sec2 += `<div id="replaceTarget" class="resultWrapper">`;
+    sec2 += `<div id="${ID_REPLACE_TARGET}" class="${CLS_REPLACE_TARGET}">`;
     sec2 += `<textarea id="before"></textarea>`;
     sec2 += `<textarea id="after"></textarea>`;
     sec2 += `</div>`;
-    sec2 += `<button type="button" >実行</button>`;
+    sec2 += `<button id="${ID_BTN_EXECUTE}" type="button" >実行</button>`;
     sec2 += `</section>`;
 
     nodes.push(htmlToNode(sec1));
