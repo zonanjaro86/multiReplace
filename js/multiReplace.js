65628120009f1b86a357bd3883d240579517294a
zonanjaro86 <zonanjaro86@gmail.com>
2019-03-04 11:57:50 +0900

    置換実行処理実装

--- js/multiReplace.js
+++ js/multiReplace.js
@@ -3,13 +3,14 @@
  */
     const ID_REPLACE_STRINGS = 'replaceStrings';
     const ID_REPLACE_TARGET = 'replaceTarget';
+    const ID_REPLACE_RESULT = 'replaceResult';
     const ID_BTN_ADDROW = 'btn_addRow';
     const ID_BTN_EXECUTE = 'btn_execute';
     const CLS_REPLACE_STRING_BEFORE = 'beforeString';
     const CLS_REPLACE_STRING_AFTER = 'afterString';
-    const CLS_REPLACE_TARGET = 'resultWrapper';
+    const CLS_REPLACE_TARGET_WRAPPER = 'resultWrapper';
 
-    const NODE_INPUT_ROW = `<li><input class="${CLS_REPLACE_STRING_BEFORE}" /><span> ⇒ </span><input class="${CLS_REPLACE_STRING_AFTER}" /></li>`;
+    const NODE_INPUT_ROW = `<li><input name="${CLS_REPLACE_STRING_BEFORE}" /><span> ⇒ </span><input name="${CLS_REPLACE_STRING_AFTER}" /></li>`;
 
 /**
  * ページ読み込み後の非同期処理起点
@@ -63,9 +64,9 @@ const createDom = () => {
     let sec2 = '';
     sec2 += `<section>`;
     sec2 += `<h2>置換対象</h2>`;
-    sec2 += `<div id="${ID_REPLACE_TARGET}" class="${CLS_REPLACE_TARGET}">`;
-    sec2 += `<textarea id="before"></textarea>`;
-    sec2 += `<textarea id="after"></textarea>`;
+    sec2 += `<div id="" class="${CLS_REPLACE_TARGET_WRAPPER}">`;
+    sec2 += `<textarea id="${ID_REPLACE_TARGET}"></textarea>`;
+    sec2 += `<textarea id="${ID_REPLACE_RESULT}"></textarea>`;
     sec2 += `</div>`;
     sec2 += `<button id="${ID_BTN_EXECUTE}" type="button" >実行</button>`;
     sec2 += `</section>`;
@@ -88,6 +89,12 @@ const setEvent = () => {
     const btn_addRow = document.getElementById(ID_BTN_ADDROW);
     if (btn_addRow) {
         btn_addRow.addEventListener('click', addInputRow);
+    }
+
+    // 実行ボタンイベント
+    const btn_execute = document.getElementById(ID_BTN_EXECUTE);
+    if (btn_execute) {
+        btn_execute.addEventListener('click', executeMultiReplace);
     }
 };
 
@@ -120,12 +127,52 @@ const addInputRow = () => {
 };
 
 /**
+ * 置換実行処理
+ */
+const executeMultiReplace = () => {
+    // 置換対象を取得
+    const targetStr = (() => {
+        const replaceTarget = document.getElementById(ID_REPLACE_TARGET);
+        if (replaceTarget && replaceTarget.value) {
+            return replaceTarget.value;
+        } else {
+            return '';
+        }
+    })();
+
+    // 置換文字列を取得
+    const replaceStrings = (() => {
+        let replaceStrings = [];
+        const lists = document.getElementById(ID_REPLACE_STRINGS).children;
+        for (const elem of lists) {
+            if (elem && elem.children) {
+                const before = elem.children.namedItem(CLS_REPLACE_STRING_BEFORE);
+                const after = elem.children.namedItem(CLS_REPLACE_STRING_AFTER);
+                if (before !== 'undefined' && after !== 'undefined') {
+                    replaceStrings.push({before:before.value, after:after.value});
+                }
+            }
+        }
+        return replaceStrings;
+    })();
+    
+    // 置換実行
+    const result = multiReplace(targetStr, replaceStrings);
+
+    // 置換結果を画面へ反映
+    const replaceResult = document.getElementById(ID_REPLACE_RESULT);
+    if (replaceResult) {
+        replaceResult.value = result;
+    }
+}
+
+/**
  * 複数文字列について置換する
  * 
  * @param {string} targetStr 
  * @param {Object[]} replaceStrings
- * @param {string} replaceStrings.before
- * @param {string} replaceStrings.after
+ * @param {string} replaceStrings[].before
+ * @param {string} replaceStrings[].after
  * @return {string}
  */
 export const multiReplace = (targetStr, replaceStrings) => {
@@ -138,9 +185,6 @@ export const multiReplace = (targetStr, replaceStrings) => {
 
 onReadyPromise()
 .then(init)
-.then(() => {
-    console.log(multiReplace('aiueokakikukeo', [{before:'a', after:'b'}, {before:'o', after:'z'}]));
-})
 .catch((e) => {
     console.error(e);
 });
